da5f0e25a417220d624a0937d33994d531cb85f5
Rayanne Caroline Antunes Ribeiri <rayanne.antunes@gmail.com>
2021-05-16 15:49:20 -0300

    templates e rotas ajustadas

--- inventorysystem/dashboard/urls.py
+++ inventorysystem/dashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/inventory', views.inventory, name='inventory'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
